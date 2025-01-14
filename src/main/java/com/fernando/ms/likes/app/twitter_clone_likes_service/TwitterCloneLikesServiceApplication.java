c303017a1fb51c86e4b99b362916fe4d17e9c7eb
Fernando <asialer05@hotmail.com>
2025-01-14 18:53:09 -0500

    proyecto base

---
+++ src/main/java/com/fernando/ms/likes/app/twitter_clone_likes_service/TwitterCloneLikesServiceApplication.java
@@ -0,0 +1,13 @@
+package com.fernando.ms.likes.app.twitter_clone_likes_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterCloneLikesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitterCloneLikesServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fernando/ms/likes/app/twitter_clone_likes_service/TwitterCloneLikesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fernando.ms.likes.app.twitter_clone_likes_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterCloneLikesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
